b35e38ebd9e2cbbd681c264551b645177e582c1c
nicolas <fernandonicolasmunoz@gmail.com>
2018-10-16 21:24:12 -0300

    acceptance test 2, complete scenarios

---
+++ src/main/java/gradle/cucumber/Bomb.java
@@ -0,0 +1,13 @@
+package gradle.cucumber;
+
+public class Bomb {
+
+    private int time;
+
+    public Bomb(int i) {
+        this.time = i;
+    }
+
+    public void explode() {
+    }
+}
--- src/test/java/gradle/cucumber/BombermanStepdefs.java
+++ src/test/java/gradle/cucumber/BombermanStepdefs.java
@@ -4,6 +4,7 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -14,6 +15,8 @@ public class BombermanStepdefs {
     private Bomberman bomberman;
     private Cell cell;
     private Included enemy;
+    private Bomb bomb;
+    private Wall melaninWall;
 
     //   Scenario: Bomberman can move to empty cell
 
@@ -56,6 +59,56 @@ public class BombermanStepdefs {
        this.bomberman = null;
        assertEquals(null,this.bomberman);
     }
+
+    //   Scenario: A bomb is lauched with a time of 3 second and destroy walls of melanin
+
+    @Given("^As a bomb with a time of 3 seconds")
+    public void as_a_bomb_with_3_seconds() throws Throwable {
+            this.bomb = new Bomb(3);
+    }
+
+    @When("^it explodes and there are melinin walls$")
+    public void it_explode_and_there_are_melinin_walls() throws Throwable {
+            this.melaninWall = new Melanin();
+            this.bomb.explode();
+    }
+
+    @Then("^destroy 3 walls of melanin$")
+    public void destroy_3_walls_of_melanin() throws Throwable {
+            this.melaninWall = null;
+            this.bomb = null;
+
+            Assert.assertEquals(null,melaninWall);
+            Assert.assertEquals(null,bomb);
+    }
+
+    //   Scenario: A bomb is lauched with a time of 3 second and destroy an enemy
+
+    @When("^it explodes and there is an enemy$")
+    public void it_explode_and_there_is_an_enemy() throws Throwable {
+
+    }
+
+    @Then("^kill the enemy by the blast  wave$")
+    public void kill_the_enemy_by_the_blast_waves() throws Throwable {
+
+    }
+
+    //   Scenario: A bomb is lauched with a time of 3 second and there is a metal wall
+
+    @When("^it explodes and there are metal walls$")
+    public void it_explode_and_there_are_metal_walls() throws Throwable {
+
+    }
+
+    @Then("^it does not destroy the metal wall$")
+    public void it_does_not_destroy_the_metal_walls() throws Throwable {
+
+    }
+
+
+
+
 
 
 }
